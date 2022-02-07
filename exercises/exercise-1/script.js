7aebb68b880c452f0c400ee6bf860c338576e4d9
Eleanor Wintram <eleanorwintram@gmail.com>
2022-02-07 22:24:28 +0000

    Typos

--- exercises/exercise-1/script.js
+++ exercises/exercise-1/script.js
@@ -3,8 +3,8 @@
 Exercise 1
 ----------
 
-- Before running the code in your browser, discuss the expected order of each loop!
-- Synchronous - Write code that loops through the array of greek gods and print the
+- Before running the code, discuss the expected order of each loop!
+- Synchronous - Write code that loops through the array of greek gods and prints the
   index numbers and values to the console, e.g. '0. Aphrodite'
 - Asynchronous - Same as before but the index and the value of the god at position
   2 in array should be printed after 2 seconds. Hint: use setTimeout()
@@ -19,4 +19,3 @@ const greekGods = [
   "Poseidon",
   "Zeus",
 ];
-
--- exercises/exercise-3/script.js
+++ exercises/exercise-3/script.js
@@ -40,8 +40,7 @@ const movies = [
 
 Task 1
 Create a function called "showMovies" that
-- iterates through the "movies" array and
-- for each movie, it creates a <p> element with the movie title and director and append it to the #allMovies div.
+- iterates through the "movies" array and for each movie, it creates a <p> element with the movie title and director and append it to the #allMovies div.
 - it sets the innerText of the #moviesNumber element to the total number of the movies in the array "movies"
 
 */
@@ -57,7 +56,7 @@ Task 2
 Create a new function called "addMovie"
 - it receives a movie object as an argument - your can create a new object for your favorite movie following using the "myMovies" objects as a guide
 - it adds the new movie to the list of movies after 2 seconds. Remember to setTimeout to achieve that
-Call addMovies to add the new movie to the list and then showMovies to see the movies added on the screen.
+Call addMovie to add the new movie to the list and then showMovies to see the movies added on the screen.
 How many movies can you see on your page?
 
 */
