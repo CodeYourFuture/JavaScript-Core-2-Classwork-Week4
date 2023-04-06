b2aaaf68df2031037155d115d860fc06cd974067
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-04-06 10:10:18 +0100

    add identifier to the iteratee

--- exercises/exercise-0/example-4.js
+++ exercises/exercise-0/example-4.js
@@ -1,6 +1,6 @@
 let num = 10;
 
-const result = [1, 2, 3, 4, 5].map(function (num) {
+const result = [1, 2, 3, 4, 5].map(function multiplyBy5(num) {
   return num * 5;
 });
 
--- exercises/exercise-0/example-5.js
+++ exercises/exercise-0/example-5.js
@@ -1,6 +1,6 @@
 let num = 10;
 
-const result = [1, 2, 3, 4, 5].map(function () {
+const result = [1, 2, 3, 4, 5].map(function multiplyBy5() {
   return num * 5;
 });
 
