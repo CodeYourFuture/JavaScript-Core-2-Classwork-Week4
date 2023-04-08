dd7eb47e17bbdd18aea519b405c7fb502347793f
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-04-08 12:50:13 +0100

    E-0-4

--- exercises/exercise-0/example-4.js
+++ exercises/exercise-0/example-4.js
@@ -5,3 +5,7 @@ const result = [1, 2, 3, 4, 5].map(function multiplyBy5(num) {
 });
 
 console.log(result);
+
+//expected output: [5, 10, 15, 20, 25]
+// map() method changes all elements in the array according to its input func
+//and that function takes its input 'num', if it wouldnt exist would take num variable that is 10
