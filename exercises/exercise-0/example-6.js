dc72860127933829db9697c27159e3a3f95cce66
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-04-08 13:08:17 +0100

    E-0-6

--- exercises/exercise-0/example-6.js
+++ exercises/exercise-0/example-6.js
@@ -8,3 +8,8 @@ function printCoordinates(point) {
 }
 
 printCoordinates({ x: 10, y: 3 });
+
+//expected output: `The y coordinate is 2`, and an error for y because it's not defined
+//x takes value 2, not 1 because 1(global) < 2(func) 
+// however if it's written like ${point.x} it would take value 10
+// same for y ${point.y} y would take 3
