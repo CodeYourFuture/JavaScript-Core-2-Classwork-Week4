172e4e6d1577ca01986a9e8644930bf236bba256
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-04-08 12:45:48 +0100

    E-0-3

--- exercises/exercise-0/example-3.js
+++ exercises/exercise-0/example-3.js
@@ -10,4 +10,4 @@ printGreeting();
 //because firstname variable is a function scoped means it's only able to be run in the function it's declared
 //however const keyword defines a global variable, so it should work
 
-//i was wrong, i got the error so const doesnt declare a global variab
+//i was wrong, i got the error so const doesnt declare a global variable
