0b7fe8411468ef32e6867470743423ca1d5e5b5b
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-04-08 12:59:21 +0100

    E-0-7

--- exercises/exercise-0/example-7.js
+++ exercises/exercise-0/example-7.js
@@ -7,5 +7,11 @@ function printCoordinates(point) {
   console.log(`The y coordinate is ${y}`);
 }
 
-console.log(printCoordinates());
+console.log(printCoordinates()); // should be only printCoordinates() instead
 console.log(y);
+
+//i think we get an error about is caused existing two console.log for first console.log
+// variable 'y' is not gonna be printed because its a function scoped, it can be printed only in the function where it's declared
+
+//i was wrong about first statament: printCoordinates is run before reach console.log() as an input
+//but got an undefined for console.log but error
