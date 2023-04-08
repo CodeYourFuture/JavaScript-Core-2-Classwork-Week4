714a4e3019daf8327571008f6bfa4f164777e108
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-04-08 14:12:03 +0100

    E-1

--- exercises/exercise-1/script.js
+++ exercises/exercise-1/script.js
@@ -8,6 +8,21 @@ function printNum() {
 
 setTimeout(printNum, 3000);
 
+let count = 100000
+while(count != 0) {
+  console.log(count)
+  count--}
 num = 42;
 
 console.log("end of script...");
+
+//expected output below:
+//starting execution...
+//10
+//end of script...
+
+//i was wrong because setTimeout runs after the code executed
+//so output is
+//starting execution...
+//end of script...
+//42
