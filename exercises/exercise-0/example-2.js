7b8200f08ce4a4f0a530b5a5656fb7cb9b53eefe
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-04-08 12:45:32 +0100

    E-0-2

--- exercises/exercise-0/example-2.js
+++ exercises/exercise-0/example-2.js
@@ -6,4 +6,5 @@ const firstName = "Moussab";
 
 printGreeting();
 
-//
+//i think printGreeting function print `Hello there, Moussab`
+// because firstName variable is a global one so the function's scope can take it, even it declared after the function
