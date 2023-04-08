b8058aef840bda50c09559d195e1e4acef50a6d8
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-04-08 12:52:31 +0100

    E-0-5

--- exercises/exercise-0/example-5.js
+++ exercises/exercise-0/example-5.js
@@ -5,3 +5,7 @@ const result = [1, 2, 3, 4, 5].map(function multiplyBy5() {
 });
 
 console.log(result);
+
+
+//expected output: [50,50,50,50,50]
+//as mention previous example , first num variablr the  multiplyBy5 find is global scoped 'num'
