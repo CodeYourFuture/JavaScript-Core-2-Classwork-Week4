ee39e770347d350bf493f516ad194bbc239afa33
softacoder <113185323+softacoder@users.noreply.github.com>
2023-04-13 17:23:54 +0100

    Update example-7.js

--- exercises/exercise-0/example-7.js
+++ exercises/exercise-0/example-7.js
@@ -9,3 +9,9 @@ function printCoordinates(point) {
 
 console.log(printCoordinates());
 console.log(y);
+
+// We start with a num variable -global variable - it is valid for all code while the second variable is only valid inside the bracket
+// we have a function with parameter point to coordinate. it takes the next variable -inside brackets -  and put into the console log with ${y}. 
+// we console log inside the bracket and print The x coordinate is 10 AND The y coordinate is 3. 
+// The y variable inside the brackets is not a global variable so console.log on row 10 cannot declare and will print undefined 
+// And console.log on row 11 will print Reference error cause y is not a global variable. 
