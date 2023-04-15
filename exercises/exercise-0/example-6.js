da3a8d872392b806e555c770d6234b40cd8d7d30
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-04-15 11:33:50 +0100

    excercise 6 updated

--- exercises/exercise-0/example-6.js
+++ exercises/exercise-0/example-6.js
@@ -1,10 +1,11 @@
 let x = 1;
 
 function printCoordinates(point) {
-  let x = 2;
 
-  console.log(`The x coordinate is ${x}`);
-  console.log(`The y coordinate is ${y}`);
+  console.log(`The x coordinate is ${point.x}`);
+  console.log(`The y coordinate is ${point.y}`);
+  console.log(`The point is ${point}`);
+
 }
 
 printCoordinates({ x: 10, y: 3 });
