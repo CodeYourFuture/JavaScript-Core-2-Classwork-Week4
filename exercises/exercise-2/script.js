74c7c55d0e47482f357bd9c79bcde9b998eb2db7
anuthapaliy <113211321+anuthapaliy@users.noreply.github.com>
2023-05-26 10:36:10 +0100

    done

--- exercises/exercise-2/script.js
+++ exercises/exercise-2/script.js
@@ -9,3 +9,19 @@ Using setTimeout and setInterval
   Hint: try searching for setInterval.
 
 */
+
+
+let colour = red;
+console.log("change background color");
+
+function changeColor() {
+  console.log(colour);
+}
+
+
+setTimeout(changeBackgroundColor, 5000);
+
+colour = green;
+
+// setTimeout(function, milliseconds ) Executes a function, after waiting a specified number of milliseconds.
+// setInterval(function, milliseconds ) Same as setTimeout(), but repeats the execution of the function continuously.
